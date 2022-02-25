f4c3031f18615b5d99694faa9f9b477ffaa08e69
marvelousayomide007 <100359501+marvelousayomide007@users.noreply.github.com>
2022-02-25 07:53:45 +0100

    Add files via upload

---
+++ c#assignment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            double d =  6.0 ;
+            string s = " federal poly ilaro ";
+
+
+            Console.WriteLine("enter the first number");
+            int first_no = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the first double ");
+            double second_no= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter a statement");
+            string statement = Console.ReadLine();
+
+
+
+
+            Console.WriteLine(i + first_no);
+
+            Console.WriteLine(d + second_no);
+
+            Console.WriteLine(s + statement);
+
+
+
+
+
+            Console.ReadLine();
+
+
+                   
+        }
+    }
+}
